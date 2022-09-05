837517e0591642c141afc9bb9e5f841cd61244b5
Alperen Aga <alperencode@gmail.com>
2022-09-05 19:18:58 +0300

    Update basics

--- basics.py
+++ basics.py
@@ -1,11 +1,10 @@
 import pynmea2
 
-file = open('nmea_sentences.log', encoding='utf-8')
+file = open('simulator_sentences.log', encoding='utf-8')
 
 for sentence in file.readlines():
     try:
-        nmea_msg = pynmea2.parse(sentence)
-        print(repr(nmea_msg))
-    except pynmea2.ParseError as e:
-        print(f"Couldn't parse {sentence}")
-        continue
+        msg = pynmea2.parse(sentence,check=False)
+        print(msg.lat)
+    except:
+        print(f"No attributes")
