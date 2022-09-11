af64abb35256b5f84a073f7e41e9c5a9a2c2194d
Alperen Aga <alperencode@gmail.com>
2022-09-11 21:27:41 +0300

    Add .gitignore

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import parsers
 
-file = open("nmea_sentences.log", "r")
+file = open("logs/nmea_sentences.log", "r")
 
 result = []
 for sentence in file.readlines():
     result.append(parsers.parse(sentence))
 
-for i in result:
-    print(i)
+for sentence in result:
+    print(sentence)
