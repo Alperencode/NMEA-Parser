21703d4a37ca354d6daf6b7195acd069d4eb9614
Alperen Aga <alperencode@gmail.com>
2022-09-18 02:35:26 +0300

    Update Data-Pased-Parser Add print logs

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from parsers import sentence_based_parser
-from parsers import data_based_parser
+from parsers import sentence_based_parser,data_based_parser
 
 file = open("logs/nmea_sentences.log", "r")
 
 result = []
 for sentence in file.readlines():
+    print("\n" +sentence)
     print(data_based_parser.parse(sentence))
+    print()
 
 for sentence in result:
     print(sentence)
--- parsers/data_based_parser.py
+++ parsers/data_based_parser.py
@@ -11,36 +11,58 @@ dataDict = {}
 def parse(sentence):
     global dataDict 
     
+    temp = sentence
+    try:
+        dataDict["Checksum"] = temp.split(",")[-1].split("*")[-1][:2]
+        print("Checksum Updated: " + str(temp.split(",")[-1].split("*")[-1][:2]))
+    except: pass
+
     try: sentence = pynmea2.parse(sentence)
     except: return dataDict
 
-    try: dataDict["Sentence Type"] = sentence.sentence_type 
+    try: 
+        dataDict["Sentence Type"] = sentence.sentence_type
+        print("Sentence Type Updated: " + sentence.sentence_type)
     except: pass
-    try: dataDict["UTC Time"] = sentence.timestamp 
+    try: 
+        dataDict["UTC Time"] = sentence.timestamp
+        print("UTC Time Updated: " + str(sentence.timestamp))
     except: pass
-    try: dataDict["Latitude"] = sentence.latitude
+    try: 
+        dataDict["Latitude"] = sentence.latitude
+        print("Latitude Updated: " + str(sentence.latitude))
     except: pass
-    try: dataDict["Longitude"] = sentence.longitude
+    try: 
+        dataDict["Longitude"] = sentence.longitude
+        print("Longitude Updated: " + str(sentence.longitude))
     except: pass
-    try: dataDict["GPS Quality"] = sentence.gps_qual
+    try: 
+        dataDict["GPS Quality"] = sentence.gps_qual
+        print("GPS Quality Updated: " + str(sentence.gps_qual))
     except: pass
-    try: dataDict["Number of Satellites"] = sentence.num_sats
+    try: 
+        dataDict["Number of Satellites"] = sentence.num_sats
+        print("Number of Satellites Updated: " + str(sentence.num_sats))
     except: pass
-    try: dataDict["Horizontal Dilution of Precision"] = sentence.horizontal_dil
+    try: 
+        dataDict["Horizontal Dilution of Precision"] = sentence.horizontal_dil
+        print("Horizontal Dilution of Precision Updated: " + str(sentence.horizontal_dil))
     except: pass
-    try: dataDict["Altitude"] = sentence.altitude
+    try: 
+        dataDict["Altitude"] = sentence.altitude
+        print("Altitude Updated: " + str(sentence.altitude))
     except: pass
-    try: dataDict["Geoidal Separation"] = sentence.geo_sep
+    try: 
+        dataDict["Geoidal Separation"] = sentence.geo_sep
+        print("Geoidal Separation Updated: " + str(sentence.geo_sep))
     except: pass
-    try: dataDict["Age of Differential GPS Data"] = sentence.age_gps_data
+    try: 
+        dataDict["Age of Differential GPS Data"] = sentence.age_gps_data
+        print("Age of Differential GPS Data Updated: " + str(sentence.age_gps_data))
     except: pass
-    try: dataDict["Differential Reference Station ID"] = sentence.ref_station_id
-    except: pass
-    try: dataDict["Checksum"] = sentence.checksum
-    except: pass
-    try: dataDict["Checksum Valid"] = sentence.is_checksum_valid()
-    except: pass
-    try: dataDict["Checksum Calculated"] = sentence.calculate_checksum()
+    try: 
+        dataDict["Differential Reference Station ID"] = sentence.ref_station_id
+        print("Differential Reference Station ID Updated: " + str(sentence.ref_station_id))
     except: pass
 
     return dataDict
