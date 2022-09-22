5da09e0bda04d8c12a982f49277859ce27219ea9
Alperen Aga <alperencode@gmail.com>
2022-09-22 17:00:27 +0300

    Update Data-Based-parsers New:     - Add debug and error logs Will do:     - Update the approach of parsing data     - Add parser for unknown sentence types     - Automate the process of parsing data

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from parsers import sentence_based_parser,data_based_parser
 
+file = open("logs/simulator_sentences.log", "r")
+
+for sentence in file.readlines():
+    print("\n" +sentence)
+    print(data_based_parser.parse(sentence))
+    print()
+
+for item,key in data_based_parser.dataDict.items():
+    print(item + ": " + str(key))
+
 file = open("logs/nmea_sentences.log", "r")
 
 for sentence in file.readlines():
@@ -9,3 +19,8 @@ for sentence in file.readlines():
 
 for item,key in data_based_parser.dataDict.items():
     print(item + ": " + str(key))
+
+print(f"\nValid Sentences: {set(data_based_parser.valid_sentences)}\n")
+print(f"Less Valid Sentences: {set(data_based_parser.less_valid_sentences)}\n")
+print(f"Unknown Sentences: {set(data_based_parser.unknown_sentences)}\n")
+print(f"Data Gathered: {set(data_based_parser.data_gathered)}\n")
--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,2 +1,3 @@
 from parsers.sentence_based_parser import *
+from parsers.data_based_parser import *
 
